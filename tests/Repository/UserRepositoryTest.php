e0190cceb93446f56d4c702feb3d34b5b7826452
farhan wahyudi <farhanwahyudi875@gmail.com>
2024-07-06 23:12:37 +0700

    feat: user update repository

--- tests/Repository/UserRepositoryTest.php
+++ tests/Repository/UserRepositoryTest.php
@@ -9,6 +9,9 @@
         private UserRepository $userRepository;
         
         public function setUp(): void {
+            $this->sessionRepository = new SessionRepository(Database::getConnection());
+            $this->sessionRepository->deleteAll();
+
             $this->userRepository = new UserRepository(Database::getConnection());
             $this->userRepository->deleteAll();
         }
@@ -31,6 +34,23 @@
         public function testFindByIdNotFound() {
             $user = $this->userRepository->findById('notfound');
             $this->assertNull($user);
+        }
+
+        public function testUpdate() {
+            $user = new User();
+            $user->id = 'hans';
+            $user->name = 'hans';
+            $user->password = 'hans1234';
+            $this->userRepository->save($user);
+
+            $user->name = 'budi';
+            $this->userRepository->update($user);
+
+            $result = $this->userRepository->findById($user->id);
+
+            $this->assertEquals($user->id, $result->id);
+            $this->assertEquals($user->name, $result->name);
+            $this->assertEquals($user->password, $result->password);
         }
     }
 ?>
